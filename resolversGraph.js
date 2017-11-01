fe77fa308b2f0a2a62e87ded86ec06916750808a
Unknown <yannik.stenzel@me.com>
2017-11-01 15:38:29 +0100

    mutations simplified, createClub added

--- resolversGraph.js
+++ resolversGraph.js
@@ -92,8 +92,7 @@ export default async function() {
     },
     Mutation: {
       registerUser(_, params) {
-        let session = driver.session()
-        let query = `
+        return setMutation(driver,params,`
           CREATE (user:User {
             name: $name, 
             email: $email, 
@@ -105,108 +104,85 @@ export default async function() {
             logins: 0,
             birthday: 373030177000,
             regKey: 'regkey??'
-          }) RETURN CASE WHEN user IS NULL THEN false ELSE true END
-        `
-        return session.run(query, params).then( result => {
-          return result.records.map( record => {
-            session.close()
-            return record;
-          })
-        }).catch( error => {
-          console.error(error.stack)
-        })
+          }) RETURN CASE WHEN user IS NULL THEN false ELSE true END`)
       },
       createCourseType(_, params) {
-        let session = driver.session()
-        let query = `
+        return setMutation(driver,params,`
           CREATE (ct:Coursetype {
             type: $type
-          }) RETURN CASE WHEN ct IS NULL THEN false ELSE true END
-        `
-        return session.run(query, params).then( result => {
-          return result.records.map( record => {
-            session.close()
-            return record;
-          })
-        }).catch( error => {
-          console.error(error.stack)
-        })
+          }) RETURN CASE WHEN ct IS NULL THEN false ELSE true END`)
       },
       createCourse(_, params) {
-        let session = driver.session()
-        let query = `
+        return setMutation(driver,params,`
           MATCH (ct:Coursetype) WHERE ID(ct) = $courseTypeId
           CREATE (course:Course {
             name: $name,
             breitengrad: $breitengrad,
             laengengrad: $laengengrad,
             info: $info
-          })-[:TYPE_OF]->(ct) RETURN CASE WHEN course IS NULL THEN false ELSE true END
-        `
-        return session.run(query, params).then( result => {
-          return result.records.map( record => {
-            session.close()
-            return record;
-          })
-        }).catch( error => {
-          console.error(error.stack)
-        })
+          })-[:TYPE_OF]->(ct) RETURN CASE WHEN course IS NULL THEN false ELSE true END`)
       },
       // createClub ??
       createGallery(_, params) {
-        return setMutation(driver,params,"gallery",
-          ` CREATE (gallery:Gallery {
-              image: $image,
-              text: $text
-            })`)
+        return setMutation(driver,params,`
+          CREATE (gallery:Gallery {
+            image: $image,
+            text: $text
+          })
+          RETURN CASE WHEN gallery IS NULL THEN false ELSE true END`)
       },
       createRound(_, params) {
-        return setMutation(driver,params,"round",`
+        return setMutation(driver,params,`
           MATCH (u:User) WHERE ID(u) = $userId
           MATCH (c:Course) WHERE ID(c) = $courseId
           CREATE (round:Round {
             date: $date
           })-[:PLAYED_BY]->(u)
-          CREATE (round)-[:PLAYED_ON]->(c)`)
+          CREATE (round)-[:PLAYED_ON]->(c)
+          RETURN CASE WHEN round IS NULL THEN false ELSE true END`)
       },
       createHole(_, params) {
-        return setMutation(driver,params,"hole",`
+        return setMutation(driver,params,`
           MATCH (r:Round) WHERE ID(r) = $roundId
           CREATE (hole:Hole {
             hole: $hole,
             strokes: $strokes
-          })-[:PLAYED_IN]->(r)`)
+          })-[:PLAYED_IN]->(r)
+          RETURN CASE WHEN hole IS NULL THEN false ELSE true END`)
       },
       createLine(_, params) {
-        return setMutation(driver,params,"line",`
+        return setMutation(driver,params,`
           MATCH (ct:Coursetype) WHERE ID(ct) = $courseTypeId
           CREATE (line:Line {
             name: $name,
             info: $info
-          })-[:TYPE_OF]->(ct)`)
+          })-[:TYPE_OF]->(ct)
+          RETURN CASE WHEN line IS NULL THEN false ELSE true END`)
       },
       addFriend(_, params) {
-        return setMutation(driver,params,"",`
+        return setMutation(driver,params,`
           MATCH (u:User) WHERE ID(u) = $id
           MATCH (friend:User {email: $email})
           CREATE (u)-[:IS_FRIEND]->(friend)`)
       },
       addLineForCourse(_, params) {
-        return setMutation(driver,params,"",`
+        return setMutation(driver,params,`
           MATCH (c:Course) WHERE ID(c) = $courseId
           MATCH (l:Line) WHERE ID(l) = $lineId
           CREATE (c)-[:IN_COURSE]->(l)`)
       },
       // does delete always return null?
       deleteRound(_,params) {
-        return setMutation(driver,params,"round",`
+        return setMutation(driver,params,`
           MATCH (round:Round)-[:PLAYED_BY]->(u:User) WHERE ID(round) = $roundId AND ID(u) = $userId
-          DETACH DELETE round`)
+          DETACH DELETE round
+          RETURN CASE WHEN round IS NULL THEN false ELSE true END`)
       },
       deleteLineFromCourse(_,params) {
-        return setMutation(driver,params,"relation",`
+        return setMutation(driver,params,`
           MATCH (c:Course)-[relation:IN_COURSE]->(l:Line) WHERE ID(c) = $courseId AND ID(l) = $lineId
-          DETACH DELETE relation`)
+          DETACH DELETE relation
+          RETURN CASE WHEN relation IS NULL THEN false ELSE true END`)
       }
     },
     LONG: GraphQLLong,
@@ -253,9 +229,9 @@ function resolveTypes(driver, params, parentType, relationName, childType) {
     })
 }
 
-function setMutation(driver,params,typeName,query) {
+function setMutation(driver,params,query) {
     let session = driver.session();
-    if (typeName !="") query += ` RETURN CASE WHEN `+typeName+` IS NULL THEN false ELSE true END`
+    // if (typeName !="") query += ` RETURN CASE WHEN `+typeName+` IS NULL THEN false ELSE true END`
     return session.run(query, params).then( result => {
       return result.records.map( record => {
         session.close()
